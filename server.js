955738ed1c8cdb7cc4cbe08c28001d82eda0fdd7
devrishu <dev.rishu.mca@gmail.com>
2021-12-17 13:42:17 -0500

    add routing

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cp = require("child_process");
 const path = require("path");
-
+const routes = require('./routes');
 
 
 const app = express();
@@ -13,34 +13,15 @@ app.set('views',path.join(__dirname,'./views'));
 
 app.use(express.static(path.join(__dirname,'./static')));
 
-app.get('/',(request,response)=>{
-    //response.sendFile(path.join(__dirname,'./static/index.html'));
-    response.render('pages/index',{pageTitle: 'Welcome' });
-});
-// app.get('/',(request,response)=>{
-//     response.send('Hellp Express :) ');
-// });
-
+app.use('/',routes());
 
 app.listen(port,() => {
     console.log(`express server running on port ${port} !`);
 });
 
-console.log("restarting");
+
 // const dirPaths = process.argv.slice(2);
 // console.log(dirPaths);
 
 // let url = "http://localhost:3000/";
 // cp.exec(`start ${url}`);
-// const createWindow = () => {
-//     const win = new BrowserWindow({
-//       width: 800,
-//       height: 600
-//     })
-  
-//     win.loadFile('index.html')
-//   }
-
-//   uiapp.whenReady().then(() => {
-//     createWindow()
-//   })
