a1ff0d53a2fa6bb1434b6d567197f8f8bcae03bd
devrishu <dev.rishu.mca@gmail.com>
2022-01-02 22:18:58 -0500

    jest unit test

---
+++ services/FileService.test.js
@@ -0,0 +1,20 @@
+const FileService = require('./FileService');
+
+
+
+describe('getDirData',() =>{
+    it('should return the file and folder details of services folder',()=>{
+        const dirPath = "services";
+        const fileService = new FileService(dirPath);
+        const expected = {"name":"services", "isDirectory" : true , "path": "services", 
+                            "files":[
+                                        {"name":"FileService.js", "isDirectory" : false , "path": "services/FileService.js","files":[]},
+                                        {"name":"FileService.test.js", "isDirectory" : false , "path": "services/FileService.test.js","files":[]},
+                                        {"name":"FileSystemChangeService", "isDirectory" : false , "path": "services/FileSystemChangeService","files":[]}
+                                    ]
+                        };
+        const actual = fileService.getDirData(dirPath,dirPath);
+        console.log(actual);
+        expect(actual).toEqual(expected);
+    });
+});
