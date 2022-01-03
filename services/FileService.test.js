4892075388f1e0917db38b9baf12035830e37b47
devrishu <dev.rishu.mca@gmail.com>
2022-01-02 22:22:41 -0500

    fileservice unit test for getList

--- services/FileService.test.js
+++ services/FileService.test.js
@@ -1,11 +1,27 @@
 const FileService = require('./FileService');
 
 
+describe('getList',() =>{
+    it('should return the file and folder details of services folder',()=>{
+        const dirPath = "services";
+        const fileService = new FileService([dirPath]);
+        const expected = {"content":[{"name":"services", "isDirectory" : true , "path": "services", 
+                            "files":[
+                                        {"name":"FileService.js", "isDirectory" : false , "path": "services/FileService.js","files":[]},
+                                        {"name":"FileService.test.js", "isDirectory" : false , "path": "services/FileService.test.js","files":[]},
+                                        {"name":"FileSystemChangeService", "isDirectory" : false , "path": "services/FileSystemChangeService","files":[]}
+                                    ]
+                        }]};
+        const actual = fileService.getList();
+        expect(actual).toEqual(expected);
+    });
+});
+
 
 describe('getDirData',() =>{
     it('should return the file and folder details of services folder',()=>{
         const dirPath = "services";
-        const fileService = new FileService(dirPath);
+        const fileService = new FileService([dirPath]);
         const expected = {"name":"services", "isDirectory" : true , "path": "services", 
                             "files":[
                                         {"name":"FileService.js", "isDirectory" : false , "path": "services/FileService.js","files":[]},
